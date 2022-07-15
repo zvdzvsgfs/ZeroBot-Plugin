4a4951baebab81ce5881f3631e1cfa30524cd32b
zvdzvsgfs <109381484+zvdzvsgfs@users.noreply.github.com>
2022-07-16 04:27:26 +0800

    DHHBDBD

--- main.go
+++ main.go
@@ -224,7 +224,7 @@ func init() {
 
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		昵称： append（[]string{*adana}，“PM”，“mhy”），
+		NickName:      append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
 		CommandPrefix: *prefix,
 		SuperUsers:    sus,
 		Driver:        []zero.Driver{config.W[0]},

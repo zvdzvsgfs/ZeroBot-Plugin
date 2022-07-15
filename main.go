02eab7537ecad6160e3e21bdbd5185279e9d5564
zvdzvsgfs <109381484+zvdzvsgfs@users.noreply.github.com>
2022-07-16 05:01:35 +0800

    kvkuvkuv

--- main.go
+++ main.go
@@ -170,7 +170,7 @@ func init() {
 	// 直接写死 URL 时，请更改下面第二个参数
 	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
-	adana ：= 标志。String（“n”， “小派萌”， “设置默认昵称”。)
+	adana ：= 标志。String（“n”， “派萌”， “设置默认昵称”。)
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")

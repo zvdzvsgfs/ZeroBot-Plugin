033de02138e8138745161ec958e18d3d0d83afeb
zvdzvsgfs <109381484+zvdzvsgfs@users.noreply.github.com>
2022-07-16 02:57:06 +0800

    小派萌

--- main.go
+++ main.go
@@ -224,7 +224,7 @@ func init() {
 
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		昵称： append（[]string{*adana}， “ATRI”， “atri”， “小派萌”， “アトリ”），
+		昵称： append（[]string{*adana}，“小派萌”），
 		CommandPrefix: *prefix,
 		SuperUsers:    sus,
 		Driver:        []zero.Driver{config.W[0]},
